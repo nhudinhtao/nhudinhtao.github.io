c937796929d35789432e7ad76707e4a7791a4135
Tao Nhu <taonhu@TaoNhus-MacBook-Pro.local>
2016-09-01 10:50:09 +0700

    add game card

---
+++ game-card/js/javascript.js
@@ -0,0 +1,48 @@
+var cards = ['JC.gif', 'JD.gif', 'JH.gif', 'JS.gif', 'KC.gif', 'KD.gif', 'KH.gif', 'KS.gif', 'N1.gif', 'N2.gif', 'QC.gif', 'QD.gif', 'QH.gif', 'QS.gif'];
+
+//autorun javascript
+(function() {
+
+  // Display initial cards
+  displayCard();
+  
+})();
+
+function displayCard() {
+	var length = cards.length;
+	cards = shuffle(cards);
+
+	// Add cards to screen
+    var html = '';
+    for (var i = 0; i < length; i++) {
+        html += '<div class="grid"><div class="card" data="' + cards[i] + '" onclick="flip(this)">' +
+        '<div class="front"><img src="img/back.gif"/></div>' +
+        '<div class="back"><img src="img/' + cards[i] + '"/></div></div></div>';
+    };
+    $('.content').html(html);
+}
+
+function flip(card) {
+	console.log(card);
+	// Flip this card
+    $(card).toggleClass('flipped');
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
