7cc25621f15706d6d10956cedbaca6c3a2d774b9
Tao Nhu <taonhu@TaoNhus-MacBook-Pro.local>
2016-09-01 12:23:59 +0700

    add sound and effect

--- game-card/js/javascript.js
+++ game-card/js/javascript.js
@@ -1,4 +1,5 @@
 var cards = ['JC.gif', 'JD.gif', 'JH.gif', 'JS.gif', 'KC.gif', 'KD.gif', 'KH.gif', 'KS.gif', 'N1.gif', 'N2.gif', 'QC.gif', 'QD.gif', 'QH.gif', 'QS.gif'];
+var current = null;
 
 //autorun javascript
 (function() {
@@ -9,13 +10,16 @@ var cards = ['JC.gif', 'JD.gif', 'JH.gif', 'JS.gif', 'KC.gif', 'KD.gif', 'KH.gif
 })();
 
 function displayCard() {
+	//duplicating array - from 14 items to 28 items
+	cards = cards.concat(cards);
+
 	var length = cards.length;
 	cards = shuffle(cards);
 
 	// Add cards to screen
     var html = '';
     for (var i = 0; i < length; i++) {
-        html += '<div class="grid"><div class="card" data="' + cards[i] + '" onclick="flip(this)">' +
+        html += '<div class="grid"><div class="card" data-name="' + cards[i] + '" onclick="flip(this)">' +
         '<div class="front"><img src="img/back.gif"/></div>' +
         '<div class="back"><img src="img/' + cards[i] + '"/></div></div></div>';
     };
@@ -23,8 +27,33 @@ function displayCard() {
 }
 
 // Flip card
-function flip(card) {	
+function flip(card) {
     $(card).toggleClass('flipped');
+
+    if (!current) {
+    	current = $(card);
+    } else {
+    	console.log(current.attr('data-name'));
+    	console.log($(card).attr('data-name'));
+    	if (current.attr('data-name') != $(card).attr('data-name')) {
+    		//different
+    		document.getElementById('incorrect-sound').play();
+    		setTimeout(function() {
+    			current.toggleClass('flipped');
+    			$(card).toggleClass('flipped');
+    			current = null;
+    		}, 500);
+    		
+    	} else {
+    		//equalize'
+    		document.getElementById('correct-sound').play();
+    		setTimeout(function() {
+    			current.css('opacity', '0');
+    			$(card).css('opacity', '0');
+    			current = null;
+    		}, 200);
+    	}
+    }
 }
 
 //Shuffle items in array
